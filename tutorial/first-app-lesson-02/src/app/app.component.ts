1dd1d15dbe985d6fe0006d8954b7915261df1023
Juan <juan@juanagui.com>
2024-01-12 16:00:33 +0100

    moves tutorials to their own folder

--- tutorial/first-app-lesson-02/src/app/app.component.ts
+++ tutorial/first-app-lesson-02/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
